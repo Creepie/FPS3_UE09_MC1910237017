ee1ff91a7f043784b181866c1e89ff532749cbb9
MarioEberth <marioeberth@gmail.com>
2021-01-16 12:38:08 +0100

    update

--- Graph.cpp
+++ Graph.cpp
@@ -6,18 +6,36 @@
 
 using namespace std;
 
+
+/**
+ * Erzeugt einen neuen Graphen
+ */
 Graph::Graph() {
 
 }
 
+/**
+ * Gibt einen vector aller eingefuegten Knoten zurueck
+ * @return a vector list of current saved Vertex
+ */
 std::vector<MyVertex> Graph::getVertices() {
     return vertices;
 }
 
+/**
+ * Gibt die Anzahl der eingefuegten Knoten zurueck
+ * @return the size of the vertices
+ */
 int Graph::getNumberOfVertices() {
     return vertices.size();
 }
 
+/**
+ * Fuegt neuen Knoten v in den Graphen ein und liefert seinen Index
+ * im Knotenvector, oder -1 wenn der Knoten bereits eingefügt ist
+ * @param v represent the given MyVertex object
+ * @return index of insertedVertex > else -1 if already exist
+ */
 int Graph::insertVertex(MyVertex v) {
     if (find(vertices.begin(), vertices.end(), v) != vertices.end()) {
         return -1;
@@ -27,6 +45,15 @@ int Graph::insertVertex(MyVertex v) {
     }
 }
 
+/**
+ * Fuegt eine ungerichtete Kante mit dem Gewicht weight zwischen den
+ * Knoten v1 und v2 ein. Die Methode liefert false
+ * falls die Kante bereits existiert, sonst true
+ * @param v1 represent the first MyVertex object
+ * @param v2 represent the second MyVertex object
+ * @param weight represent the weight of the edge
+ * @return true if inserted, else false
+ */
 bool Graph::insertEdge(MyVertex v1, MyVertex v2, int weight) {
     if ((find(vertices.begin(), vertices.end(), v1) != vertices.end())) {
         if (find(vertices.begin(), vertices.end(), v2) != vertices.end()) {
@@ -43,6 +70,13 @@ bool Graph::insertEdge(MyVertex v1, MyVertex v2, int weight) {
     return false;
 }
 
+/**
+ * this method is to compare if a Edge is the same
+ * @param v1 represent the first MyVertex object
+ * @param v2 represent the second MyVertex object
+ * @param weight is the given weight
+ * @return true if already exists, else false
+ */
 bool Graph::compareEdge(MyVertex v1, MyVertex v2, int weight) {
     for (int i = 0; i < edges.size(); ++i) {
         if (edges.at(i).weight == weight && edges.at(i).in == v1 && edges.at(i).out == v2) {
@@ -53,6 +87,12 @@ bool Graph::compareEdge(MyVertex v1, MyVertex v2, int weight) {
     }
 }
 
+/**
+ * this method return the weight of an Edge
+ * @param v1 represent the first MyVertex object
+ * @param v2 represent the second MyVertex object
+ * @return the weight if exists, else -1
+ */
 int Graph::getEdgeWeight(MyVertex v1, MyVertex v2) {
     for (int i = 0; i < edges.size(); i++) {
         if (edges.at(i).in == v1 && edges.at(i).out == v2) {
@@ -62,7 +102,11 @@ int Graph::getEdgeWeight(MyVertex v1, MyVertex v2) {
     return -1;
 }
 
-
+/**
+ * Liefert eine NxN Adjazenzmatrix für diesen Graphen, wobei N = getNumVertices().
+ * Die Matrix enthält die Gewichte der Kanten (>=0).
+ * @return a double adjacencyMatrix
+ */
 std::vector<std::vector<int>> Graph::getAdjacencyMatrix() {
     vector<vector<int>> matrix(vertices.size(), vector<int>(vertices.size(), -1));
     for (int i = 0; i < vertices.size(); ++i) {
@@ -81,6 +125,11 @@ std::vector<std::vector<int>> Graph::getAdjacencyMatrix() {
     return matrix;
 }
 
+/**
+ * Liefert einen Vector von Knoten welche adjazent zum Knoten v sind.
+ * @param v is the given MyVertex Object
+ * @return a vector of adjacentVertices
+ */
 std::vector<MyVertex> Graph::getAdjacentVertices(MyVertex v) {
     vector<MyVertex> vector;
 
